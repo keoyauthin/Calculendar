ec7bd932fd0fbc35b11b4b422207db6b3098fd27
Kenneth Fechter <kenneth.fechter@gmail.com>
2016-11-29 15:24:41 -0500

    moved fab behavior class back to original location

--- app/src/main/java/com/kennethfechter/datepicker/adapters/FABScrollBehavior.java
+++ app/src/main/java/com/kennethfechter/datepicker/adapters/FABScrollBehavior.java
@@ -1,4 +1,4 @@
-package com.kennethfechter.datepicker.DataLayer;
+package com.kennethfechter.datepicker.adapters;
 
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
